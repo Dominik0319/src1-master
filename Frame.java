0185e48b9ef39eefda8a2e1f284da0160703952b
Dominik0319 <dominik.skrobisz1@gmail.com>
2021-02-22 16:30:44 +0100

    first commit

---
+++ Frame.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Frame extends JFrame{
+    private static final long serialVersionUID = 1L;
+    public Frame(){
+        super("Kółko i krzyżyk");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(640,400);
+        setLocation(600,50);
+        
+        JPanel buttons = new Buttons();
+        add(buttons);
+        
+        
+    
+        setVisible(true);
+        
+    }
+}
